860e9ce67b68107ec9ec0963eeef6cb7470f2f13
thanhlongzzz <longvipzzz@gmail.com>
2020-08-15 11:31:16 +0700

    test push

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send("Hello world")
+    var text = "Test string"
+    res.send(text)
 })
 
 app.listen(port, () => {
