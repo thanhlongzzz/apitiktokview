07f3942141d81f1b73b650c3543c0c4b5b6d2b7c
thanhlongzzz <longvipzzz@gmail.com>
2020-08-15 11:17:26 +0700

    test comit

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express()
 const port = 3000
 
-app.get('/',(req,res)=>{
-    res.send("hello word 1")
+app.get('/', (req, res) => {
+    res.send("Hello world")
 })
 
-app.listen(port,()=>{
-    console.log("Listening at ${port}");
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
