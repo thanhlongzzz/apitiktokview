aab677e88d3b398a5a2369d1c11fdbb79317f8aa
thanhlongzzz <longvipzzz@gmail.com>
2020-08-15 12:25:36 +0700

    deploy to heroku

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env)
 })
